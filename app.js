26d5be5803e244286ac5d72fe4ee6b8932ed11dd
laraib shahzad <laraibshahzad9@gmail.com>
2023-09-07 18:34:38 +0500

    add all files

---
+++ app.js
@@ -0,0 +1,183 @@
+// Chapter 1 
+
+// Question 1
+
+alert("First Name");
+
+alert("Last Name");
+
+alert("Email");
+
+alert("Phone Number");
+
+alert("Password");
+
+// Question 2 
+
+alert("You're learning JavaScript!");
+
+// Question 3 
+
+alert("Hello\nHow are you")
+
+// End of chp 1 
+
+// Chapter 2 
+
+// Question 1 
+
+var yourName;
+
+// Question 2 
+
+var myNAme;
+
+myNAme = "My name is Laraib Shahzad";
+
+// Question 3 
+
+var teamName = "PAKISTAN"
+
+alert(teamName);
+
+// Question 4 
+
+var bestMan = "Charlie";
+
+bestMan = "He is good at archery";
+
+// End of chp 2 
+
+// Chapter 3 
+
+// Question 1 
+
+var caseQty;
+
+// Question 2 
+
+caseQty = 144; 
+
+// Question 3 
+
+var num = 9 + 10;
+
+console.log(num)
+
+// Question 4 
+
+var number;
+
+number = 2 + 2;
+
+console.log(number)
+
+// Question 5 
+
+var merchTotal = 100;
+
+var shippingCharge = 10;
+
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal)
+
+// Value of orderTotal is 110;
+
+// Question 6 
+
+var firstNum = 38;
+
+var res = firstNum + 15;
+
+console.log(res)
+
+// End of chp 3 
+
+// Chapter 4 
+
+// Question 1 
+
+var productcost = 3.45;
+
+// Question 2 
+
+var nameOfBand;
+
+// Question 3 
+
+var legalNumber = 45;
+
+// Question 4 
+
+var combineNumber = productcost + legalNumber; 
+
+console.log(combineNumber)
+
+// Question 5 
+
+var userName;
+
+var Email;
+
+var legal5Variable;
+
+// var last name;
+
+// var @main;
+
+// var 3 illegal variable;
+
+// End of chp 4 
+
+// Chapter 5 
+
+// Question 1 
+
+// The Name of the arithmetic operator that gives you the ramiander is modulas and the symbol is %.
+
+// Question 2 
+
+var num = 20 % 6;
+
+console.log(num)
+
+// The value of num is 2.
+
+// Question 3 
+
+var largeNum = 1000 * 2000;
+
+console.log(largeNum)
+
+// Question 4 
+
+var firstNumber = 65;
+
+var secondNumber = 24;
+
+var result = firstNumber - secondNumber;
+
+console.log(result);
+
+// Question 5 
+
+var thirdNumber = 8;
+
+var result1 = thirdNumber / 2;
+
+console.log(result1)
+
+// Question 6 
+
+var string = "The multiplication of 2 and 9 is"
+
+var multiply = 2;
+
+var multiply2 = 9;
+
+var multiplyResult = multiply * multiply2;
+
+alert(string + " " + multiplyResult)
+
+// End of chp 5
